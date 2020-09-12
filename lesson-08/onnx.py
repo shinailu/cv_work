3cbe0708b7176a6215894aa3f786cd0d8677aa06
1807588139@qq.com <1807588139@qq.com>
2020-09-13 00:20:52 +0800

    新增onnx;pd模型转化

--- lesson-08/onnx.py
+++ lesson-08/onnx.py
@@ -8,6 +8,7 @@ model = resnet34(num_classes=6, shortcut_type=True, sample_size=128, sample_dura
 weights = '../model/ct_pos_recogtion_20191115125435/ct_pos_recognition_0009_best.pth'
 model.load_state_dict(torch.load(weights))
 
+
 dummy_input = torch.randn(1,1,128,128,128)
 
 torch.onnx.export(model, dummy_input, 'ctPosRecognition.onnx', verbose=True, input_names=['input'], output_names=['output'])
